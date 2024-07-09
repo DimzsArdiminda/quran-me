454cf90011d6dfd6b91668cb3c2caf4b8dd15050
Dimas Ardiminda <113449449+DimzsArdiminda@users.noreply.github.com>
2024-07-09 14:06:12 +0700

    on going

--- src/app/component/Home.tsx
+++ src/app/component/Home.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import DetailSurah from './DetailSurah';
 import Link from 'next/link';
 
 type Surah = {
@@ -35,6 +34,7 @@ async function getSurah(): Promise<Surah[]> {
 export default function Home() {
   const [surah, setSurah] = useState<Surah[]>([]);
   const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
@@ -43,6 +43,8 @@ export default function Home() {
         setSurah(data);
       } catch (error: any) {
         setError(error.message);
+      } finally {
+        setIsLoading(false);
       }
     }
 
@@ -51,52 +53,50 @@ export default function Home() {
 
   return (
     <>
-    <main className="mt-10">
-      <h4 style={{ fontSize: '2rem' }} className="text-center box-decoration-clone">
-        Quran Indonesia
-      </h4>
-
-      <div className="container mx-auto mt-6">
-        <div className="card bordered shadow-lg mb-6">
-          <div className="card-body">
-            <h2 className="card-title">QuranME</h2>
-            <p className="text-gray-700">
-              Al-Qur'an adalah kitab suci umat Islam yang di dalamnya terdapat ajaran-ajaran yang harus diikuti oleh umat Islam.
-            </p>
-          </div>
+      {isLoading ? (
+        <div className="flex justify-center items-center h-screen">
+          <button type="button" className="btn loading">Loading...</button>
         </div>
+      ) : (
+        <main className="mt-10">
+          <h4 style={{ fontSize: '2rem' }} className="text-center box-decoration-clone">
+            QuranME Indonesia
+          </h4>
 
-        <div className="grid grid-cols-1 py-6 px-1 md:grid-cols-3 gap-4">
-          {error ? (
-            <div className="col-span-1 md:col-span-3 text-red-500">{error}</div>
-          ) : (
-            surah.map((surat) => (
-              // <DetailSurah surat={surat}/>
-              <div key={surat.nomor} className="card bordered shadow-lg">
-                <div className="card-body">
-                  <Link href='detail-surat/'>
-                    <h2 className="card-title">{surat.nama_latin} ({surat.nama})</h2>
-                    <p className="text-gray-700" dangerouslySetInnerHTML={{ __html: surat.deskripsi }}></p>
-                    <p className="text-gray-700">Jumlah Ayat: {surat.jumlah_ayat}</p>
-                    <p className="text-gray-700">Tempat Turun: {surat.tempat_turun}</p>
-                    <p className="text-gray-700">Arti: {surat.arti}</p>
-                    </Link>
-                    <div>
-                    {/* <div className="container">
-                      <h3 className="card-title">Audio</h3>
-                      <audio controls>
-                      <source src={surat.audio} type="audio/mpeg" />
-                      Your browser does not support the audio element.
-                      </audio>
-                      </div> */}
-                  </div>
-                </div>
+          <div className="container mx-auto mt-6">
+            <div className="card bordered shadow-lg mb-6">
+              <div className="card-body">
+                <h2 className="card-title">Quran</h2>
+                <p className="text-gray-700">
+                  Al-Qur'an adalah kitab suci umat Islam yang di dalamnya terdapat ajaran-ajaran yang harus diikuti oleh umat Islam.
+                </p>
               </div>
-            ))
-          )}
-        </div>
-      </div>
-    </main>
+            </div>
+
+            <div className="grid grid-cols-1 py-6 px-1 md:grid-cols-3 gap-4">
+              {error ? (
+                <div className="col-span-1 md:col-span-3 text-red-500">{error}</div>
+              ) : (
+                surah.slice(0, 3).map((surat) => (
+                  <div key={surat.nomor} className="card bordered shadow-lg">
+                    <div className="card-body">
+                      <Link href={`/detail-surat/${surat.nomor}`}>
+                        
+                          <h2 className="card-title">{surat.nama_latin} ({surat.nama})</h2>
+                          <p className="text-gray-700">Nomor urut surat: {surat.nomor}</p>
+                          <p className="text-gray-700" dangerouslySetInnerHTML={{ __html: surat.deskripsi }}></p>
+                          <p className="text-gray-700">Jumlah Ayat: {surat.jumlah_ayat}</p>
+                          <p className="text-gray-700">Tempat Turun: {surat.tempat_turun}</p>
+                          <p className="text-gray-700">Arti: {surat.arti}</p>
+                      </Link>
+                    </div>
+                  </div>
+                ))
+              )}
+            </div>
+          </div>
+        </main>
+      )}
     </>
   );
 }
